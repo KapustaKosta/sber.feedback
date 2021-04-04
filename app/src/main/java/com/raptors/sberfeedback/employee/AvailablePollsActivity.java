1e26aadfec84b9675db44e5bf21e129e641b13c1
Konstantin Starikov <k.starikov@lenta.ru>
2021-04-04 11:35:14 +0400

    second commit

---
+++ app/src/main/java/com/raptors/sberfeedback/employee/AvailablePollsActivity.java
@@ -0,0 +1,16 @@
+package com.raptors.sberfeedback.employee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.raptors.sberfeedback.R;
+
+public class AvailablePollsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available_polls);
+    }
+}
---
+++ app/src/main/java/com/raptors/sberfeedback/employer/CreatePollActivity.java
@@ -0,0 +1,16 @@
+package com.raptors.sberfeedback.employer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.raptors.sberfeedback.R;
+
+public class CreatePollActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_poll);
+    }
+}

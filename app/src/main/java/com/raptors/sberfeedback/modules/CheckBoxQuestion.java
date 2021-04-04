e4af186cfa2a2ad17bf8749e1a19d56fcddd3d52
Gliiin <Dmitriy.s2006@yandex.ru>
2021-04-04 12:15:42 +0400

    -

--- app/src/main/java/com/raptors/sberfeedback/modules/CheckBoxQuestion.java
+++ app/src/main/java/com/raptors/sberfeedback/modules/CheckBoxQuestion.java
@@ -3,7 +3,7 @@ package com.raptors.sberfeedback.modules;
 import android.widget.CheckBox;
 import java.util.ArrayList;
 
-public class CheckBoxQuestion  {
+public class CheckBoxQuestion extends Question {
     private ArrayList<String> variants;
     private ArrayList<CheckBox> checkBoxes;
     private ArrayList<String> answers;
--- app/src/main/java/com/raptors/sberfeedback/modules/TextQuestion.java
+++ app/src/main/java/com/raptors/sberfeedback/modules/TextQuestion.java
@@ -2,7 +2,7 @@ package com.raptors.sberfeedback.modules;
 
 import android.widget.EditText;
 
-public class TextQuestion {
+public class TextQuestion extends Question {
     private EditText editText;
     private String answer;
 }
